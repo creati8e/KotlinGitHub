9b0293df42960a7b6006bfbc64723a4aa6cac9d1
User <schuprin@smedialink.com>
2017-04-21 11:06:41 +0300

    generic db source

--- app/src/main/java/chuprin/serg/kotlin_github/app/data/repository/credentials/CredentialsDbSource.kt
+++ app/src/main/java/chuprin/serg/kotlin_github/app/data/repository/credentials/CredentialsDbSource.kt
@@ -1,28 +1,10 @@
 package chuprin.serg.kotlin_github.app.data.repository.credentials
 
-import chuprin.serg.kotlin_github.app.data.Source
 import chuprin.serg.kotlin_github.app.data.entity.GithubAccount
-import chuprin.serg.kotlin_github.app.data.repository.specification.DbSpecification
-import chuprin.serg.kotlin_github.app.data.repository.specification.Specification
+import chuprin.serg.kotlin_github.app.data.repository.GenericDbSource
 import com.vicpin.krealmextensions.delete
-import com.vicpin.krealmextensions.save
-import com.vicpin.krealmextensions.saveAll
-import rx.Observable
 
-@Suppress("UNCHECKED_CAST")
-class CredentialsDbSource : Source<GithubAccount> {
-
-    override fun putAll(models: List<GithubAccount>) = models.saveAll()
-
-    override fun put(model: GithubAccount) = model.save()
-
-    override fun get(specification: Specification): Observable<GithubAccount> {
-        return (specification as DbSpecification<GithubAccount>).toDbResults()
-    }
-
-    override fun getList(specification: Specification): Observable<List<GithubAccount>> {
-        return (specification as DbSpecification<List<GithubAccount>>).toDbResults()
-    }
+class CredentialsDbSource : GenericDbSource<GithubAccount>() {
 
     override fun delete(model: GithubAccount) = model.delete { model.token }
 }
